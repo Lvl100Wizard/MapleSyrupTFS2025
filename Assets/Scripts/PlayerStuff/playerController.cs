132f0f8ac2955cbfea6e4b23da09545ab0afed99
Jacob Sands <jacob.sands@torontofilmschool.ca>
2025-03-31 14:42:07 -0400

    basic player controller

---
+++ Assets/Scripts/PlayerStuff/playerController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+   [SerializeField] private float moveSpeed = 5f;
+   [SerializeField] private Rigidbody rb;
+    [SerializeField] private float turnSpeed = 360;
+
+    private Vector3 _input;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        GatherInput();
+        Look();
+
+    }
+    //fixed update for 
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void GatherInput()
+    {
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    }
+
+    void Look()
+    {
+
+        //this prevents snapping to the default angle
+        if (_input == Vector3.zero) return;
+
+
+        //basically rotating the player model based on input so they can travel in that direction
+        var relative = (transform.position + _input) - transform.position;
+        var rot = Quaternion.LookRotation(relative, Vector3.up);
+
+
+        //lerp for smooth rotation
+        transform.rotation = Quaternion.RotateTowards(transform.rotation,rot, turnSpeed * Time.deltaTime);
+    }
+
+    void Move()
+    {
+
+        //move character in forward direction based on rotation and if input is present
+        rb.MovePosition(transform.position + (transform.forward * _input.magnitude) * moveSpeed * Time.deltaTime);
+    }
+
+}
