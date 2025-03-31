1fbecbc372cca4c22fd6d585a2650436cce3b25d
Jacob Sands <jacob.sands@torontofilmschool.ca>
2025-03-31 15:01:39 -0400

    moved + tweaked camera script

--- Assets/Scripts/PlayerStuff/CameraFollow.cs
+++ Assets/Scripts/PlayerStuff/CameraFollow.cs
@@ -14,7 +14,7 @@ public class CameraFollow : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (playerCharacter == null) return;
 
