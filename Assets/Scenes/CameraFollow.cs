3f1047bf9db98941570d065cd08040031bd6349e
aidenwitjes <150625938+aidenwitjes@users.noreply.github.com>
2025-03-31 14:50:55 -0400

    CameraFollow
    Basic isometric camera follow script

---
+++ Assets/Scenes/CameraFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerCharacter;
+    public float movementSpeed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerCharacter == null) return;
+
+        transform.position = Vector3.Lerp(transform.position, playerCharacter.position, movementSpeed * Time.deltaTime);
+    }
+}
