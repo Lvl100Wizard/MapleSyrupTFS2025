3774756658fa42bc2a313f94cab634b34cd94391
aidenwitjes <150625938+aidenwitjes@users.noreply.github.com>
2025-04-03 13:00:12 -0400

    Rewards from Orders + WinScene Credits Scroll
    Added a credits scrolling script to our WinScene for the temporary credits, need tweaking for specifics. Hard coded rewards based off the scriptable object name, i.e., Order_1 -> 50$ (arbitrary values atm that need adjusting)

--- Assets/Scripts/Orders/OrderManager.cs
+++ Assets/Scripts/Orders/OrderManager.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class OrderManager : MonoBehaviour
 {
     public static OrderManager instance;
-
     public OrderData[] orderQueue;
     private int currentOrderIndex = 0;
 
@@ -24,15 +23,46 @@ public class OrderManager : MonoBehaviour
 
     public void CompleteOrder()
     {
-        if (currentOrderIndex < orderQueue.Length - 1)
+        if (currentOrderIndex < orderQueue.Length)
         {
+            OrderData currentOrder = orderQueue[currentOrderIndex];
+            float reward = GetOrderReward(currentOrder.name); // Use ScriptableObject name
+
+            // Grant money using the Wallet inside GameManager
+            GameManager.Instance.ReceiveMoney((int)reward);  // Cast to int as your ReceiveMoney method takes an int
+
+            Debug.Log($"Order {currentOrder.name} completed! Player earned ${reward}");
+
             currentOrderIndex++;
-            Debug.Log($"Next Order: Order {currentOrderIndex + 1} started.");
+
+            if (currentOrderIndex < orderQueue.Length)
+                Debug.Log($"Next Order: {orderQueue[currentOrderIndex].name} started.");
+            else
+            {
+                Debug.Log("All orders completed!");
+                FadeManager.instance.FadeToScene("WinScene");
+            }
         }
-        else
+    }
+
+    private float GetOrderReward(string orderName)
+    {
+        switch (orderName)
         {
-            Debug.Log("All orders completed!");
-            FadeManager.instance.FadeToScene("WinScene");
+            case "Order_1": return 50f;
+            case "Order_2": return 100f;
+            case "Order_3": return 150f;
+            case "Order_4": return 200f;
+            case "Order_5": return 250f;
+            case "Order_6": return 300f;
+            case "Order_7": return 400f;
+            case "Order_8": return 500f;
+            case "Order_9": return 600f;
+            case "Order_10": return 1000f;
+
+            default:
+                Debug.LogWarning($"Order {orderName} has no predefined reward!");
+                return 0f;
         }
     }
 }
